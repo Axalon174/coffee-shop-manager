073f3df718ff24b8d81738fa9c25eb6e0c1924de
Axel Verduzco <l19640239@piedad.tecnm.mx>
2026-01-25 17:36:57 -0600

    first commit

---
+++ src/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+// Creamos una única instancia del cliente para toda la app
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
